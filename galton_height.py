d77444b301dcea315cc076f6ec939d33294fd9ab
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-08-22 14:52:20 -0500

    Clarify variable names

--- galton_height.py
+++ galton_height.py
@@ -5,7 +5,9 @@ Analysis of Galton's height data.
 
 from pathlib import Path
 import time
+import math
 
+import scipy.stats as stats
 import datasense as ds
 import pandas as pd
 
@@ -51,7 +53,11 @@ def main():
         df=df_galton_female_children,
         file_in=galton_path
     )
-    print(df_galton_female_children["child_height"].describe(percentiles=percentiles))
+    print(
+        df_galton_female_children["child_height"].describe(
+            percentiles=percentiles
+        )
+    )
     print()
     print("Analysis of male children")
     print("=========================")
@@ -63,7 +69,11 @@ def main():
         df=df_galton_male_children,
         file_in=galton_path
     )
-    print(df_galton_male_children["child_height"].describe(percentiles=percentiles))
+    print(
+        df_galton_male_children["child_height"].describe(
+            percentiles=percentiles
+        )
+    )
     print()
     print("Graphs of child heights")
     print("=======================")
@@ -94,17 +104,21 @@ def main():
     print("=================================")
     print()
     sample_differences = []
+    male_children_averages = []
+    female_children_averages = []
     for i in range(replicates):
         sample = df_galton_families.sample(
             frac=fraction,
             replace=True
         )
         male_children_average = (
-            sample.loc[sample["child_sex"]=="M", "child_height"].mean()
+            sample.loc[sample["child_sex"] == "M", "child_height"].mean()
         )
+        male_children_averages.append(male_children_average)
         female_children_average = (
-            sample.loc[sample["child_sex"]=="F", "child_height"].mean()
+            sample.loc[sample["child_sex"] == "F", "child_height"].mean()
         )
+        female_children_averages.append(female_children_average)
         sample_differences.append(
             male_children_average - female_children_average
         )
@@ -120,6 +134,67 @@ def main():
     ax.set_ylabel(ylabel="Sample fraction")
     fig.savefig(fname=histogram_children_heights_differences, format="svg")
     ds.html_figure(file_name=histogram_children_heights_differences)
+    # Two-sample t test of the series
+    print("Two-sample t test of the two series")
+    print("===================================")
+    print()
+    series1 = pd.Series(data=male_children_averages)
+    series1_ave = series1.mean()
+    series1_n = series1.count()
+    series1_var = series1.var()
+    series2 = pd.Series(data=female_children_averages)
+    series2_ave = series2.mean()
+    series2_n = series2.count()
+    series2_var = series2.var()
+    ds.two_sample_t(
+        series1=series1,
+        series2=series2
+    )
+    # Two-sample t test of the differences
+    print("Two-sample t test of the differences")
+    print("====================================")
+    print()
+    delta_one_two = math.fabs(
+        series1_ave - series2_ave
+    )
+    va = series1_var / series1_n
+    vb = series2_var / series2_n
+    vc = series1_n + series2_n - 2
+    ve = math.sqrt(
+        1 / series1_n + 1 / series2_n
+    )
+    t_critical_equal = stats.t.isf(alpha / 2, vc)
+    standard_deviation_pooled = math.sqrt(
+        ((series1_n - 1) * series1_var +
+         (series2_n - 1) * series2_var) / vc
+    )
+    lower_limit = delta_one_two - t_critical_equal * \
+                  standard_deviation_pooled * ve
+    upper_limit = delta_one_two + t_critical_equal * \
+                  standard_deviation_pooled * ve
+    if 0 < lower_limit or 0 > upper_limit:
+        print(
+             'The two averages are statistically, '
+             'significantly different '
+             'because 0 is not contained within the '
+             'confidence interval ' +
+             '(' + str(lower_limit.round(3)) + ', ' +
+             str(upper_limit.round(3)) + ')'
+             ' of the difference ' +
+             str(round(number=delta_one_two, ndigits=3)) + '.'
+        )
+    else:
+        print(
+            'The two averages are not statistically, '
+             'significantly different '
+             'because 0 is contained within the '
+             'confidence interval ' +
+             '(' + str(lower_limit.round(3)) + ', ' +
+             str(upper_limit.round(3)) + ')'
+             ' of the difference ' +
+             str(round(number=delta_one_two, ndigits=3)) + '.'
+        )
+    print()
     stop_time = time.perf_counter()
     ds.script_summary(
         script_path=Path(__file__),
